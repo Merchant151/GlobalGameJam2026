0a53780347579240b9594d80763f5c0ca33bcb6b
Merchant151 <jmiller32@ggc.edu>
2026-01-30 22:48:40 -0500

    some ideas

---
+++ mask/script/Manager.gd
@@ -0,0 +1,17 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print('Hello World')
+	## Start game. 
+	#start UI 
+	#Start anyTImers and such
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func spawn_NPC():
+	pass
