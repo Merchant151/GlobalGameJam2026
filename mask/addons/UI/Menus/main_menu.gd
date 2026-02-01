71c0713c499dd579b60f4cf2629af481cda6cc50
kyleelg <47234217+kyleelg@users.noreply.github.com>
2026-01-31 22:39:11 -0500

    Created Win/Lose Panel
    Added the panels for win, and lose condition, and added a return to menu button.

--- mask/addons/UI/Menus/main_menu.gd
+++ mask/addons/UI/Menus/main_menu.gd
@@ -1,27 +1,33 @@
 extends Control
+@onready var menu_panel: Panel = $PanelContainer/MenuPanel
+@onready var credits_panel: Panel = $PanelContainer/CreditsPanel
 
-@onready var main_buttons: VBoxContainer = $PanelContainer/MainButtons
-@onready var credits_panel: Panel = $Credits Panel
 # Called when the node enters the scene tree for the first time.
 func _ready(): #Similiar to start in unity or beginplay in unreal
-	main_buttons.visible = true
+	menu_panel.visible = true
 	credits_panel.visible = false
-
-	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-func _on_start_pressed():
-	get_tree().change_scene_to_file("res://game.tscn")
-	
 
-func _on_credit_pressed():
-	main_buttons.visible = false
+func _on_start_btn_pressed():
+	print("Start")
+	get_tree().change_scene_to_file("res://game.tscn")
+
+
+func _on_credits_btn_pressed():
+	print("credits pressed")
+	menu_panel.visible = false
 	credits_panel.visible = true
 
 
-func _on_exit_pressed():
+func _on_quit_btn_pressed():
 	get_tree().quit()
+
+
+func _on_back_btn_pressed():
+	menu_panel.visible = true
+	credits_panel.visible = false
